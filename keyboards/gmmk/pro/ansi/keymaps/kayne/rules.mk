b366e63a569892feed94ea28e92b1668317fcf1c
Marcin Bratek <github@bratekmarcin.eu>
2021-10-24 14:21:43 +0200

    added my customized keymapping based on seajaysec

---
+++ keyboards/gmmk/pro/ansi/keymaps/kayne/rules.mk
@@ -0,0 +1,24 @@
+COMBO_ENABLE = yes
+COMMAND_ENABLE = yes
+CONSOLE_ENABLE = yes
+EXTRAKEY_ENABLE = yes          # Audio control and System control
+MOUSEKEY_ENABLE = no
+RAW_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_LEDMAPS = yes
+STARTUP_NUMLOCK_ON = yes
+UNICODE_ENABLE = yes
+VIA_ENABLE = no
+WPM_ENABLE = no
+KEY_OVERRIDE_ENABLE = yes
+
+SRC += utils.c seajaysec.c process_record.c encoder.c
+
+ifeq ($(strip $(WPM_ENABLE)), yes)
+	SRC += fun.c
+endif
+
+ifeq ($(strip $(RGB_MATRIX_LEDMAPS)), yes)
+	SRC += rgb_matrix_ledmaps.c
+	OPT_DEFS += -DRGB_MATRIX_LEDMAPS_ENABLED
+endif
